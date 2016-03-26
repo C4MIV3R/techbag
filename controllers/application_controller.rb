545db2fcedaca3c2576faa0e8905c6c5fad281c9
cam iverson <camiver@gmail.com>
2016-03-26 15:59:49 -0500

    added user and app controllers with basic routing functions

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < Sinatra::Base
 
-
+  ActiveRecord::Base.establish_connection(
+    :adapter    => 'postgresql',
+    :database   => 'tech_bags'
+  )
 
   set :views, File.expand_path('../../views', __FILE__)
   set :public_folder, File.expand_path('../../public', __FILE__)
+
+
+  not_found do
+    erb :notfound
+  end
 
   get '/' do
     erb :index
