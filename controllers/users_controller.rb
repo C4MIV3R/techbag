8dcc3e6fed3cd0407d5d21db6aa1a7d964a0b7ef
cam iverson <camiver@gmail.com>
2016-03-26 16:47:52 -0500

    Bcrypt now working for registration of users

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -4,4 +4,20 @@ class UsersController < ApplicationController
     erb :login
   end
 
+  post '/login' do
+    
+  end
+
+  post '/register' do
+    hashed_password = BCrypt::Password.create(params[:password_hash])
+    @user = User.create({
+        :username         => params[:username],
+        :email            => params[:email],
+        :password_hash    => hashed_password
+      })
+    # puts Rainbow(params).magenta
+    # puts Rainbow(hashed_password).yellow
+    redirect '/'
+  end
+
 end
